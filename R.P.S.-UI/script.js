a86780df070ac4430d976fb68accee9ecb587133
Nadson Mendes <nadsonfbm@hotmail.com>
2022-12-10 03:13:28 -0300

    html and style

--- R.P.S.-UI/script.js
+++ R.P.S.-UI/script.js
@@ -1,10 +1,16 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+const playerWinsDisplay = document.getElementById('player-wins');
+const computerWinsDisplay = document.getElementById('computer-wins');
+const drawsDisplay = document.getElementById('draws');
+let userChoice;
+let computerChoice;
+let result;
+let playerWins = 0;
+let computerWins = 0;
+let draws = 0;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
@@ -27,27 +33,46 @@ function computerPlay() {
     computerChoiceDisplay.innerHTML = computerChoice
   }
 
+let win = "Great choice! Rock crushes Scissor! You won the round!";
+let loss = "Oh no! Paper covers Rock! You lost the round!";
+let draw = "Two heads thinking alike. It's a tied round.";
+
 function playRound() {
     if (userChoice === "Rock" && computerChoice === "Paper") {
-        result = "Oh no! Paper covers Rock! You lost the round!";
+        result = loss;
     }
     if (userChoice === "Rock" && computerChoice === "Scissor") {
-        result = "Great choice! Rock crushes Scissor! You won the round!";
+        result = win;
     }
     if (userChoice === "Scissor" && computerChoice === "Rock") {
-        result = "Oh no! Rock crushes Scissor! You lost the round!";
+        result = loss;
     }
     if (userChoice === "Scissor" && computerChoice === "Paper") {
-        result = "Great choice! Scissor cuts Paper! You won the round!";
+        result = win;
     }
     if (userChoice === "Paper" && computerChoice === "Scissor") {
-        result = "Oh no! Scissor cuts Paper! You lost the round!";
+        result = loss;
     }
     if (userChoice === "Paper" && computerChoice === "Rock") {
-        result = "Great choice! Paper covers Rock! You won the round!";
+        result = win;
     }
     if (userChoice === computerChoice) {
-        result = "Two heads thinking alike. It's a tied round.";
+        result = draw;
     }
     resultDisplay.innerHTML = result;
+}
+
+function results() {
+        if (result === win){
+            playerWinsDisplay.innerHTML = playerWins;
+            playerWins + 1;
+        } 
+        if (result === loss){
+            computerWinsDisplay.innerHTML = computerWins;
+            computerWins + 1;
+        }
+        if (result === draw){
+            drawsDisplay.innerHTML = draws;
+            draws + 1;
+        }
 }
